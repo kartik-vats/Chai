1e4cfe85ec8ef132f4dc0b2cc5537bd27b04f78c
kartik-vats <ks25152005@gmail.com>
2025-08-14 15:36:13 +0530

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -25,19 +25,19 @@ if (process.env.GITHUB_ID && process.env.GITHUB_SECRET) {
 export default NextAuth({
   providers,
   callbacks: {
-    async jwt({ token, account }) {
-      if (account && !token.providerAccountId) {
-        token.providerAccountId = account.providerAccountId;
-        token.provider = account.provider;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (session.user && token.providerAccountId) {
-        session.user.providerAccountId = token.providerAccountId;
-        session.user.provider = token.provider;
-      }
-      return session;
-    },
+  async jwt({ token, account }) {
+    if (account) {
+      token.providerAccountId = account.providerAccountId as string;
+      token.provider = account.provider as string;
+    }
+    return token;
   },
+  async session({ session, token }) {
+    if (session.user) {
+      session.user.providerAccountId = token.providerAccountId;
+      session.user.provider = token.provider;
+    }
+    return session;
+  },
+}
 });
